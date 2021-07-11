56aff159484511d672146f6dd11cadd19f2e866c
nugraha-abd <nugrahaabdurahman@gmail.com>
2021-07-11 14:20:41 +0700

    add addBookHandler routing

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
 import {
-
+  addBookHandler,
+  getAllBooksHandler,
 } from './handler.js'
 
 const routes = [
-
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  }
 ]
 
 export { routes }
