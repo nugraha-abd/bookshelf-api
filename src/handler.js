2cbda2773d57a2b8822c5dc38d6030f39bcd391d
nugraha-abd <nugrahaabdurahman@gmail.com>
2021-07-11 15:27:27 +0700

    add getBookByIdHandler() function handler

--- src/handler.js
+++ src/handler.js
@@ -76,9 +76,27 @@ const getAllBooksHandler = () => ({
   },
 })
 
-const getBookByIdHandler = (request, h) => ({
+const getBookByIdHandler = (request, h) => {
+  const { bookId } = request.params
 
-})
+  const book = books.filter((book) => book.id === bookId)[0]
+
+  if(book !== undefined) {
+    return {
+      status: 'success',
+      data: {
+        book,
+      },
+    }
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  })
+  response.code(404)
+  return response
+}
 
 export {
   addBookHandler,
